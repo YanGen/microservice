7786500ed92749ba62ec5482d71f804610ec186f
young <zorag@qq.com>
2019-12-10 11:10:04 +0800

    number 号码

---
+++ microservicecloud/microservicecloud-provider-number-18001/src/main/java/com/muhuan/springcloud/NumberProvider18001_App.java
@@ -0,0 +1,13 @@
+package com.muhuan.springcloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class NumberProvider18001_App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ microservicecloud/microservicecloud-provider-number-18001/src/main/java/com/muhuan/springcloud/config/CorsConfig.java
@@ -0,0 +1,40 @@
+package com.muhuan.springcloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @ClassName CorsConfig
+ * @Description TODO
+ * @Author geng <yg.db@uniteddata.com>
+ * @Date 2019/12/3
+ * @Version 1.0.0
+ **/
+@Configuration
+public class CorsConfig {
+    @Value("${cors.addAllowedOrigin}")
+    private String addAllowedOrigin;
+    @Value("${cors.addAllowedHeader}")
+    private String addAllowedHeader;
+    @Value("${cors.addAllowedMethod}")
+    private String addAllowedMethod;
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin(addAllowedOrigin); // 1 设置访问源地址
+        corsConfiguration.addAllowedHeader(addAllowedHeader); // 2 设置访问源请求头
+        corsConfiguration.addAllowedMethod(addAllowedMethod); // 3 设置访问源请求方法
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4 对接口配置跨域设置
+        return new CorsFilter(source);
+    }
+}
